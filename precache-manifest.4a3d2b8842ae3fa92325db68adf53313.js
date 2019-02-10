ec3c3f9181107d99455ef51304aa3d07500526e5
Sid Moparthi <siddhu.moparthi@gmail.com>
2019-02-10 14:22:56 -0500

    Updates

---
+++ precache-manifest.4a3d2b8842ae3fa92325db68adf53313.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/aurum-fx/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "41327226665d5ee93bae",
+    "url": "/aurum-fx/static/js/runtime~main.41327226.js"
+  },
+  {
+    "revision": "78b32fcb90ffbf785669",
+    "url": "/aurum-fx/static/js/main.78b32fcb.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/aurum-fx/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "78b32fcb90ffbf785669",
+    "url": "/aurum-fx/static/css/main.23dfe706.chunk.css"
+  },
+  {
+    "revision": "b0ffacfd4b1d1d102819603a3adfc7a3",
+    "url": "/aurum-fx/index.html"
+  }
+];
